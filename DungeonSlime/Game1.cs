4e420301e6fe9939428e0e96f3e7d382fdd80568
Jacob Snover <snoverjacob@yahoo.com>
2026-07-11 00:11:43 -0400

    Add texture atlas system and refactor sprite rendering
    Introduced TextureAtlas and TextureRegion classes for managing and drawing sprite regions from a single texture. Updated Content.mgcb to include atlas.png. Refactored Game1.cs to use the new atlas system and draw "slime" and "bat" sprites. Commented out old logo rendering logic. Updated project file. This improves sprite management and rendering efficiency.

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -2,25 +2,28 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
 
 namespace DungeonSlime;
 
 public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-    private Texture2D _logo;
+    //private GraphicsDeviceManager _graphics;
+    //private SpriteBatch _spriteBatch;
+    //private Texture2D _logo;
     //private int x = 90;
     //private int y = 90;
     //private int _horDirection = 1;
     //private int _verDirection = 0;
-    private int _angle = 0;
+    //private int _angle = 0;
     //private float _scale = 0.5f;
-    private float _scaleX = 0.5f;
-    private float _scaleY = 1.5f;
-    private float _scaleModifier = 0.1f;
-    private float _opacity = 0.0f;
+    //private float _scaleX = 0.5f;
+    //private float _scaleY = 1.5f;
+    //private float _scaleModifier = 0.1f;
+    //private float _opacity = 0.0f;
 
+    private TextureRegion _slime;
+    private TextureRegion _bat;
     public Game1()
         : base("Dungeon Slime", 1280, 720, false)
     {
@@ -37,18 +40,22 @@ public class Game1 : Core
     protected override void LoadContent()
     {
         // TODO: use this.Content to load your game content here
-        _logo = Content.Load<Texture2D>("Images/logo");
+        //_logo = Content.Load<Texture2D>("Images/logo");
 
-        base.LoadContent();
+        Texture2D atlasTexture = Content.Load<Texture2D>("Images/atlas");
+        TextureAtlas atlas = new TextureAtlas(atlasTexture);
+        atlas.AddRegion("slime", 0, 0, 20, 20);
+        atlas.AddRegion("bat", 20, 0, 20, 20);
+        _slime = atlas.GetRegion("slime");
+        _bat = atlas.GetRegion("bat");
+
+        //base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // Clear the back buffer.
-        GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your update logic here
         //if (x >= 140 && y >= 140)
@@ -76,83 +83,90 @@ public class Game1 : Core
         //y += _verDirection;
 
         // rotate the logo by 1 degree each frame
-        if (_angle == 360)
-        {
-            _angle = 0;
-        }
-        else
-        {
-            _angle += 1;
-        }
+        //if (_angle == 360)
+        //{
+        //    _angle = 0;
+        //}
+        //else
+        //{
+        //    _angle += 1;
+        //}
 
         // scale the logo up and down between 0.5 and 1.5
-        if (_scaleX >= 1.5f)
-        {
-            _scaleModifier = -0.01f;
-        }
-        else if (_scaleX <= 0.5f)
-        {
-            _scaleModifier = 0.01f;
-        }
+        //if (_scaleX >= 1.5f)
+        //{
+        //    _scaleModifier = -0.01f;
+        //}
+        //else if (_scaleX <= 0.5f)
+        //{
+        //    _scaleModifier = 0.01f;
+        //}
 
-        _scaleX += _scaleModifier;
-        _scaleY += -_scaleModifier;
-        _opacity += _scaleModifier;
+        //_scaleX += _scaleModifier;
+        //_scaleY += -_scaleModifier;
+        //_opacity += _scaleModifier;
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.Black);
+        GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // the bounds of the icon within the texture
-        Rectangle icon = new Rectangle(0, 0, 128, 128);
+        //// the bounds of the icon within the texture
+        //Rectangle icon = new Rectangle(0, 0, 128, 128);
 
-        // the bounds of the word mark within the texture
-        Rectangle wordMark = new Rectangle(150, 34, 458, 58);
+        //// the bounds of the word mark within the texture
+        //Rectangle wordMark = new Rectangle(150, 34, 458, 58);
 
         // TODO: Add your drawing code here
         // begin the sprite batch with SpriteSortMode.FrontToBack to allow for layerDepth sorting
-        SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        // Draw the slime texture region at a scale of 4.0
+        _slime.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 0.0f);
+
+        // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 1.0f);
+
 
         // use middle of the texture as the origin for rotation
         // use Vector2 for scale with dynamic scaling in x and y directions
         // use Color.White * _opacity for opacity with dynamic opacity
         // use rectangle for sourceRectangle to draw the icon portion of the texture separately from the word mark portion of the texture
         // using layerDepth to draw the icon on top of the word mark
-        SpriteBatch.Draw(
-        _logo,                      // texture
-        new Vector2(                // position
-            Window.ClientBounds.Width,
-            Window.ClientBounds.Height) * 0.5f,
-        icon,                       // sourceRectangle
-        Color.White * _opacity,                // color
-        MathHelper.ToRadians(_angle), // rotation
-        new Vector2(                // origin
-            icon.Width,
-            icon.Height) * 0.5f,
-        new Vector2(_scaleX, _scaleY),          // scale
-        SpriteEffects.None,         // effects
-        1.0f                        // layerDepth
-        );
+        //SpriteBatch.Draw(
+        //_logo,                      // texture
+        //new Vector2(                // position
+        //    Window.ClientBounds.Width,
+        //    Window.ClientBounds.Height) * 0.5f,
+        //icon,                       // sourceRectangle
+        //Color.White * _opacity,                // color
+        //MathHelper.ToRadians(_angle), // rotation
+        //new Vector2(                // origin
+        //    icon.Width,
+        //    icon.Height) * 0.5f,
+        //new Vector2(_scaleX, _scaleY),          // scale
+        //SpriteEffects.None,         // effects
+        //1.0f                        // layerDepth
+        //);
 
 
-        SpriteBatch.Draw(
-        _logo,                      // texture
-        new Vector2(                // position
-            Window.ClientBounds.Width,
-            Window.ClientBounds.Height) * 0.5f,
-        wordMark,                   // sourceRectangle
-        Color.White * (1.0f - _opacity),                // color
-        MathHelper.ToRadians(_angle), // rotation
-        new Vector2(                // origin
-            wordMark.Width,
-            wordMark.Height) * 0.5f,
-        new Vector2(_scaleX, _scaleY),          // scale
-        SpriteEffects.None,         // effects
-        0.0f                        // layerDepth
-        );
+        //SpriteBatch.Draw(
+        //_logo,                      // texture
+        //new Vector2(                // position
+        //    Window.ClientBounds.Width,
+        //    Window.ClientBounds.Height) * 0.5f,
+        //wordMark,                   // sourceRectangle
+        //Color.White * (1.0f - _opacity),                // color
+        //MathHelper.ToRadians(_angle), // rotation
+        //new Vector2(                // origin
+        //    wordMark.Width,
+        //    wordMark.Height) * 0.5f,
+        //new Vector2(_scaleX, _scaleY),          // scale
+        //SpriteEffects.None,         // effects
+        //0.0f                        // layerDepth
+        //);
 
         // use middle of the texture as the origin for rotation
         // use Vector2 for scale with dynamic scaling in x and y directions
---
+++ MonoGameLibrary/Graphics/TextureAtlas.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MonoGameLibrary.Graphics;
+
+public class TextureAtlas
+{
+
+    private Dictionary<string, TextureRegion> _regions;
+
+    /// <summary>
+    /// Gets or sets the texture used for this texture atlas.
+    /// </summary>
+    public Texture2D Texture { get; set; }
+
+    /// <summary>
+    /// Gets the dictionary of texture regions in this texture atlas, where the key is the name of the region and the value is the corresponding <see cref="TextureRegion"/> object.
+    /// </summary>
+    public TextureAtlas()
+    {
+        _regions = new Dictionary<string, TextureRegion>();
+    }
+
+    /// <summary>
+    /// Creates a new texture atlas instance using the given texture.
+    /// </summary>
+    /// <param name="texture">The source texture represented by the texture atlas.</param>
+    public TextureAtlas(Texture2D texture)
+    {
+        Texture = texture;
+        _regions = new Dictionary<string, TextureRegion>();
+    }
+
+    /// <summary>
+    /// Adds a new texture region to the atlas with the specified name and rectangle coordinates.
+    /// </summary>
+    /// <param name="name">The name of the texture region.</param>
+    /// <param name="x">The x-coordinate of the texture region.</param>
+    /// <param name="y">The y-coordinate of the texture region.</param>
+    /// <param name="width">The width of the texture region.</param>
+    /// <param name="height">The height of the texture region.</param>
+    public void AddRegion(string name, int x, int y, int width, int height)
+    {
+        var region = new TextureRegion(Texture, x, y, width, height);
+        _regions.Add(name, region);
+    }
+
+    /// <summary>
+    /// Retrieves the texture region associated with the specified name from the atlas.
+    /// </summary>
+    /// <param name="name">The name of the texture region to retrieve.</param>
+    /// <returns>The texture region associated with the specified name.</returns>
+    public TextureRegion GetRegion(string name)
+    {
+        return _regions[name];
+    }
+
+    /// <summary>
+    /// Removes the texture region associated with the specified name from the atlas.
+    /// </summary>
+    /// <param name="name">The name of the texture region to remove.</param>
+    /// <returns>true if the texture region was found and removed; otherwise, false.</returns>
+    public bool RemoveRegion(string name)
+    {
+        return _regions.Remove(name);
+    }
+
+    /// <summary>
+    /// Clears all texture regions from the atlas.
+    /// </summary>
+    public void Clear()
+    {
+        _regions.Clear();
+    }
+
+    /// <summary>
+    /// Creates a new texture atlas instance from an XML file that defines the texture and its regions.
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    public static TextureAtlas FromFile(ContentManager content, string fileName)
+    {
+        TextureAtlas atlas = new TextureAtlas();
+
+        string filePath = Path.Combine(content.RootDirectory, fileName);
+
+        using (Stream stream = TitleContainer.OpenStream(filePath))
+        {
+            using (XmlReader reader = XmlReader.Create(stream))
+            {
+                XDocument doc = XDocument.Load(reader);
+                XElement root = doc.Root;
+
+                // The <Texture> element contains the content path for the Texture2D to load.
+                // So we will retrieve that value then use the content manager to load the texture.
+                string texturePath = root.Element("Texture").Value;
+                atlas.Texture = content.Load<Texture2D>(texturePath);
+
+                // The <Regions> element contains individual <Region> elements, each one describing
+                // a different texture region within the atlas.  
+                //
+                // Example:
+                // <Regions>
+                //      <Region name="spriteOne" x="0" y="0" width="32" height="32" />
+                //      <Region name="spriteTwo" x="32" y="0" width="32" height="32" />
+                // </Regions>
+                //
+                // So we retrieve all of the <Region> elements then loop through each one
+                // and generate a new TextureRegion instance from it and add it to this atlas.
+                var regions = root.Element("Regions")?.Elements("Region");
+
+                if (regions != null)
+                {
+                    foreach(var region in regions)
+                    {
+                        string name = region.Attribute("Name")?.Value;
+                        int x = int.Parse(region.Attribute("X")?.Value ?? "0");
+                        int y = int.Parse(region.Attribute("Y")?.Value ?? "0");
+                        int width = int.Parse(region.Attribute("Width")?.Value ?? "0");
+                        int height = int.Parse(region.Attribute("Height")?.Value ?? "0");
+
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            atlas.AddRegion(name, x, y, width, height);
+                        }
+                    }
+                }
+
+                return atlas;
+            }
+        }
+    }
+}
